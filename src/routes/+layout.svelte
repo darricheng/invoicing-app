766508bd512965a9a71c809113d64f01af796198
Darric Heng <darricheng@gmail.com>
2024-02-05 22:00:47 +0800

    create base layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,34 @@
 <script>
+	import { AppShell, AppBar, TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
 	import '../app.pcss';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<TabGroup
+					justify="justify-center"
+					active="variant-filled-primary"
+					hover="hover:variant-soft-primary"
+					flex="flex-1 lg:flex-none"
+					rounded=""
+					border=""
+					class="bg-surface-100-800-token w-full"
+				>
+					<TabAnchor href="/">
+						<svelte:fragment>Home</svelte:fragment>
+					</TabAnchor>
+					<TabAnchor href="/generate-invoices">
+						<svelte:fragment>Generate Invoices</svelte:fragment>
+					</TabAnchor>
+					<TabAnchor href="/edit-data">
+						<svelte:fragment>Edit Data</svelte:fragment>
+					</TabAnchor>
+				</TabGroup>
+			</svelte:fragment>
+			<svelte:fragment slot="trail"></svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+</AppShell>
