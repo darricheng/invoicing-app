05a1611cb943dc11ae5226be09f594eec1720b3c
Darric Heng <darricheng@gmail.com>
2024-02-07 21:37:33 +0800

    add initial pages in app

---
+++ src/routes/edit-data/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { invoke } from '@tauri-apps/api';
+
+	async function addCustomer() {
+		console.log('clicked add customer');
+		const data = await invoke('list_customers');
+		console.log(data);
+	}
+</script>
+
+<div class="p-8">
+	<div class="mb-8 grid grid-cols-2">
+		<h1 class="h1">Edit Customers</h1>
+		<div class="flex flex-row-reverse">
+			<button type="button" class="variant-filled btn" on:click={addCustomer}>Add Customer</button>
+		</div>
+	</div>
+	<!-- <Table bind:source={tableSimple} /> -->
+</div>
---
+++ src/routes/generate-invoices/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { type TableSource, tableMapperValues, Table } from '@skeletonlabs/skeleton';
+
+	function generateInvoices() {
+		console.log('clicked generate invoices');
+	}
+
+	const sourceData = [
+		{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+		{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+		{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+		{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+		{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' }
+	];
+
+	let tableSimple: TableSource = {
+		// A list of heading labels.
+		head: ['Name', 'Symbol', 'Weight'],
+		// The data visibly shown in your table body UI.
+		body: tableMapperValues(sourceData, ['name', 'symbol', 'weight']),
+		// Optional: The data returned when interactive is enabled and a row is clicked.
+		meta: tableMapperValues(sourceData, ['position', 'name', 'symbol', 'weight']),
+		// Optional: A list of footer labels.
+		foot: ['Total', '', '<code class="code">5</code>']
+	};
+</script>
+
+<div class="p-8">
+	<div class="mb-8 grid grid-cols-2">
+		<h1 class="h1">Budget</h1>
+		<div class="flex flex-row-reverse">
+			<button type="button" class="variant-filled btn" on:click={generateInvoices}
+				>Generate Invoices</button
+			>
+		</div>
+	</div>
+	<Table bind:source={tableSimple} />
+</div>
