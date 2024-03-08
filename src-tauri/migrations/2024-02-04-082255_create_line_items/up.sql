f0cc2da9a966bf631e5b8a29208ea6186a5de405
Darric Heng <darricheng@gmail.com>
2024-03-08 21:14:32 +0800

    add details field to db

--- src-tauri/migrations/2024-02-04-082255_create_line_items/up.sql
+++ src-tauri/migrations/2024-02-04-082255_create_line_items/up.sql
@@ -3,6 +3,7 @@ CREATE TABLE line_items (
     id INTEGER PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
     rate REAL NOT NULL,
+    details TEXT,
     customer_id INTEGER NOT NULL,
     FOREIGN KEY (customer_id)
         REFERENCES customers(id)
