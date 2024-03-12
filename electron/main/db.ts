effba56a890e3f24dd1f373f660a3543bb1d79c0
Darric Heng <darricheng@gmail.com>
2024-03-12 19:25:18 +0800

    add functionality for getEverything

--- electron/main/db.ts
+++ electron/main/db.ts
@@ -2,6 +2,7 @@ import { IpcMainInvokeEvent } from 'electron';
 import { Customer, FullCustomerForm, FullCustomerWithLineItems, LineItem } from './types';
 import { DataTypes, Sequelize } from 'sequelize';
 import { is } from '@electron-toolkit/utils';
+import { Model } from 'sequelize';
 
 // function signatures should match the types in the main/types.ts file
 // with the addition of the IpcMainInvokeEvent argument
@@ -113,5 +114,25 @@ export async function editCustomer(
 ): Promise<[number, number]> {}
 export async function deleteCustomer(_e: IpcMainInvokeEvent, id: number): Promise<number> {}
 export async function getEverything(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   _e: IpcMainInvokeEvent
-): Promise<Array<FullCustomerWithLineItems>> {}
+): Promise<Array<FullCustomerWithLineItems>> {
+  console.log('calling getEverything');
+  const data = await sequelize.models.Customer.findAll({ include: sequelize.models.LineItem });
+  return data.map((el) => {
+    const customer = el.dataValues;
+    delete customer.createdAt;
+    delete customer.updatedAt;
+    const line_items = customer.LineItems.map((el: Model<any, any>) => {
+      const item = el.dataValues;
+      delete item.createdAt;
+      delete item.updatedAt;
+      return item;
+    });
+    delete customer.LineItems;
+    return {
+      customer,
+      line_items,
+    };
+  });
+}
