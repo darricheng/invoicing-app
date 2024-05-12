255022bb5200e3d6bcdc8ecb1cf74e4769b1e97e
Darric Heng <darricheng@gmail.com>
2024-05-12 10:36:14 +0800

    copy over sveltekit files

--- svelte-app/src/app.d.ts
+++ svelte-app/src/app.d.ts
@@ -1,9 +1,41 @@
+import {
+  type AddCustomer,
+  type DeleteCustomer,
+  type EditCustomer,
+  type GetCustomer,
+  type GetEverything,
+  type ListCustomers,
+  type SendInvoices,
+} from '../electron/main/types.ts';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-// and what to do when importing types
-declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+declare global {
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
+}
+
+export interface IDbAPI {
+  listCustomers: ListCustomers;
+  getCustomer: GetCustomer;
+  addCustomer: AddCustomer;
+  editCustomer: EditCustomer;
+  deleteCustomer: DeleteCustomer;
+  getEverything: GetEverything;
+}
+
+export interface IPdfAPI {
+  sendInvoices: SendInvoices;
+}
+
+declare global {
+  interface Window {
+    dbAPI: IDbAPI;
+    pdfAPI: IPdfAPI;
+  }
 }
---
+++ svelte-app/src/lib/types.ts
@@ -0,0 +1,43 @@
+export interface CustomerFormData {
+  customer: {
+    id: number | null;
+    name: string;
+    phone: string;
+  };
+  line_items: Array<{ name: string; rate: number; id: number | null; details: string }>;
+}
+
+export interface Customer {
+  id: number;
+  name: string;
+  phone: string;
+}
+
+export interface LineItem {
+  id: number;
+  name: string;
+  rate: number;
+  customer_id: number;
+  details: string;
+}
+
+interface InvoiceLineItem {
+  id: number;
+  name: string;
+  rate: number;
+  customer_id: number;
+  quantity: number;
+  amount: number;
+  details: string;
+}
+
+export interface InvoiceTableData {
+  customer: Customer;
+  line_items: Array<InvoiceLineItem>;
+  selected: boolean;
+}
+
+export interface GenerateInvoicesData {
+  invoiceData: Array<InvoiceTableData>;
+  message: string;
+}
