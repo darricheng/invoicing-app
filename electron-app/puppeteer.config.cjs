93deaf965582514b44ea1e4e94c66659dcaa9f3e
Darric Heng <darricheng@gmail.com>
2024-05-28 22:35:27 +0800

    add puppeteer config file

---
+++ electron-app/puppeteer.config.cjs
@@ -0,0 +1,8 @@
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+// module.exports = {
+//   skipChromeDownload: true,
+//   skipDownload: true,
+//   skipChromeHeadlessShellDownload: true,
+// };
