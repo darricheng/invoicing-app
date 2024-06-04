2e8d74c36a100886af8660f5fe50bfeac6fe4126
Darric Heng <darricheng@gmail.com>
2024-06-04 21:17:55 +0800

    init appData file

---
+++ electron-app/src/main/appData.ts
@@ -0,0 +1,4 @@
+// NOTE: This file will contain all the logic pertaining to data in the local data folder.
+// E.g. creating the sqlite data file the first time, or managing chromium installations.
+// Get the appropriate folder path with app.getPath();
+// See: https://www.electronjs.org/docs/latest/api/app#appgetpathname
