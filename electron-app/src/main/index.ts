38cb107cfb32e47cfea6379b46b5aea31a7b63dc
Darric Heng <darricheng@gmail.com>
2024-06-16 15:27:07 +0800

    organise imports

--- electron-app/src/main/index.ts
+++ electron-app/src/main/index.ts
@@ -1,6 +1,8 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron';
 import { join } from 'path';
+
+import { app, shell, BrowserWindow, ipcMain } from 'electron';
 import { electronApp, optimizer, is } from '@electron-toolkit/utils';
+
 import icon from '../../resources/icon.png?asset';
 import {
   addCustomer,
