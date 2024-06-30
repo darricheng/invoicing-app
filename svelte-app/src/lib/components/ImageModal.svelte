8ce48e7b463657297e272665b7a475c7712e5cec
Darric Heng <darricheng@gmail.com>
2024-06-30 12:10:24 +0800

    show login qr properly

--- svelte-app/src/lib/components/ImageModal.svelte
+++ svelte-app/src/lib/components/ImageModal.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
-  import type { SvelteComponent } from 'svelte';
-
   // Stores
   import { getModalStore } from '@skeletonlabs/skeleton';
 
-  // Props
-  /** Exposes parent props to this component. */
-  export let parent: SvelteComponent;
-
   const modalStore = getModalStore();
 
-  const cImage = 'max-w-[90%] max-h-[90%] rounded-container-token overflow-hidden shadow-xl';
+  const cImage = 'w-modal-slim rounded-container-token overflow-hidden shadow-xl';
 </script>
 
 {#if $modalStore[0]}
-  <!-- <img src={$modalStore[0]?.image} class={cImage} alt="Example" /> -->
-  <div class={cImage}>{$modalStore[0]?.image}</div>
+  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+  <div class={cImage}>{@html $modalStore[0]?.image}</div>
 {/if}
