112852980284c9942266e858745cc1042af22a1c
Darric Heng <darricheng@gmail.com>
2024-07-04 21:06:18 +0800

    init settings page

--- svelte-app/src/routes/settings/+page.svelte
+++ svelte-app/src/routes/settings/+page.svelte
@@ -1,5 +1,29 @@
+<script lang="ts">
+</script>
+
 <div class="p-8">
   <div class="mb-8 grid grid-cols-2">
     <h1 class="h1">Settings</h1>
+  </div>
+  <div class="table-container">
+    <table class="table table-hover">
+      <tbody>
+        <tr>
+          <th>Company Name</th>
+          <td>some company name</td>
+          <td>Edit Button</td>
+        </tr>
+        <tr>
+          <th>Company Phone Number</th>
+          <td>81234567</td>
+          <td>Edit Button</td>
+        </tr>
+        <tr>
+          <th>Company Logo</th>
+          <td>a logo here</td>
+          <td>Edit Button</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </div>
